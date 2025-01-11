4637ef843ed0ff5053912d1a17ffef22afca9aab
Dominik Wombacher <dominik@wombacher.cc>
2025-01-11 12:56:23 +0100

    feat: repo 'chart-fluent-bit-syslog-to-aws-cloudtrail-data' added

---
+++ repo_chart-fluent-bit-syslog-to-aws-cloudtrail-data.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "chart-fluent-bit-syslog-to-aws-cloudtrail-data" {
+  source      = "./modules/repos"
+  repo_name   = "chart-fluent-bit-syslog-to-aws-cloudtrail-data"
+  description = "Helm Chart to receive Syslog events and push to AWS Cloudtrail Data via Fluent Bit"
+}
