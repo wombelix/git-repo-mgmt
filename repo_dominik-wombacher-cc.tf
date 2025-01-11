fed1028308785da9a74ae79d50489af6780cdf62
Dominik Wombacher <dominik@wombacher.cc>
2025-01-11 12:24:06 +0100

    feat: repo 'dominik-wombacher-cc' added

---
+++ repo_dominik-wombacher-cc.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "dominik-wombacher-cc" {
+  source      = "./modules/repos"
+  repo_name   = "dominik-wombacher-cc"
+  description = "Website: dominik.wombacher.cc, The Wombelix Post"
+}
