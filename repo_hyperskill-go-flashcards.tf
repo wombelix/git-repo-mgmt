9019f02785f733e1945c6e9af03e010fc9ad7f86
Dominik Wombacher <dominik@wombacher.cc>
2025-01-12 14:43:43 +0100

    feat: repo 'hyperskill-go-flashcards' added

---
+++ repo_hyperskill-go-flashcards.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "hyperskill-go-flashcards" {
+  source      = "./modules/repos"
+  repo_name   = "hyperskill-go-flashcards"
+  description = "My solution of Hyperskill Project Flashcards (Go)"
+}
