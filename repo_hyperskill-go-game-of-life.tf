fa35de31bbf7fa3ab6bf2e86c0ef0cccb4d76962
Dominik Wombacher <dominik@wombacher.cc>
2025-01-12 14:38:51 +0100

    feat: repo 'hyperskill-go-game-of-life' added

---
+++ repo_hyperskill-go-game-of-life.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "hyperskill-go-game-of-life" {
+  source      = "./modules/repos"
+  repo_name   = "hyperskill-go-game-of-life"
+  description = "My solution of Hyperskill Project Game of Life (Go)"
+}
