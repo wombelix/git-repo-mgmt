0cec0d897c13bdf07d6e0b52cb9784fbb1598097
Dominik Wombacher <dominik@wombacher.cc>
2025-02-01 12:57:07 +0100

    feat: repo 'aws-neuron-driver' added

---
+++ repo_aws-neuron-driver.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "aws-neuron-driver" {
+  source      = "./modules/repos"
+  repo_name   = "aws-neuron-driver"
+  description = "GPL-2.0 source code of the AWS Neuron Driver, a Linux kernel device driver supporting the AWS Neuron SDK"
+}
