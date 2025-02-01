4786d52255a8b1eebc50400cfc26304848612b3c
Dominik Wombacher <dominik@wombacher.cc>
2025-02-01 12:56:51 +0100

    feat: repo 'aws-neuron-driver-publish-source' added

---
+++ repo_aws-neuron-driver-publish-source.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: MIT
+
+module "aws-neuron-driver-publish-source" {
+  source      = "./modules/repos"
+  repo_name   = "aws-neuron-driver-publish-source"
+  description = "Command line tool to publish the source code of new AWS Neuron Driver releases"
+}
