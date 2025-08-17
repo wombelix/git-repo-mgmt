91e3d0d52a610d6304d2cadd9ce84a2eba509fba
Dominik Wombacher <dominik@wombacher.cc>
2025-08-17 16:19:43 +0200

    chore: Delete unused / outdated repos 'aws-organizations-policies' and 'aws-sideprojects-infrastructure'

--- repo_aws-organizations-policies.tf
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
-#
-# SPDX-License-Identifier: MIT
-
-module "aws-organizations-policies" {
-  source      = "./modules/repos"
-  repo_name   = "aws-organizations-policies"
-  description = "Copy of applied SCP and settings in my AWS Organization"
-}
--- repo_aws-sideprojects-infrastructure.tf
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
-#
-# SPDX-License-Identifier: MIT
-
-module "aws-sideprojects-infrastructure" {
-  source      = "./modules/repos"
-  repo_name   = "aws-sideprojects-infrastructure"
-  description = "Infrastructure-as-Code (IaC) for my sideprojects at AWS"
-}
