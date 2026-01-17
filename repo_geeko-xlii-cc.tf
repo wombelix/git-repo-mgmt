aebac33d1a9c186f87ee11e88af7c9f1c8ea308b
Dominik Wombacher <dominik@wombacher.cc>
2026-01-17 16:16:59 +0100

    feat(repo): Enable ssh key feature for geeko-xlii-cc
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_geeko-xlii-cc.tf
+++ repo_geeko-xlii-cc.tf
@@ -6,6 +6,11 @@ module "geeko-xlii-cc" {
   source      = "./modules/repos"
   repo_name   = "geeko-xlii-cc"
   description = "Server config: geeko.xlii.cc"
+
+  enable_aws_ssh_key          = true
+  enable_aws_iam_role         = true
+  enable_aws_opentofu_backend = true
+  enable_aws_ssm_read         = true
 }
 
 moved {
