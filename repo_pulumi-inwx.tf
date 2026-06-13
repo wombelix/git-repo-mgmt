5e6c71aa1a1be591bea40bc5976bb91d7367c6fa
Dominik Wombacher <dominik@wombacher.cc>
2026-06-14 00:26:44 +0200

    feat(repo): add pulumi-inwx
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

---
+++ repo_pulumi-inwx.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module "pulumi-inwx" {
+  source      = "./modules/repos"
+  repo_name   = "pulumi-inwx"
+  description = "Pulumi provider for INWX that allows registration and management of domains and their domain contacts"
+}
