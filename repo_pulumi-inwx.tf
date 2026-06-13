944fca508e8ac661d83a07319d517c5146708a3a
Dominik Wombacher <dominik@wombacher.cc>
2026-06-14 00:50:14 +0200

    feat(repo): pulumi-inwx archived
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_pulumi-inwx.tf
+++ repo_pulumi-inwx.tf
@@ -6,6 +6,7 @@ module "pulumi-inwx" {
   source      = "./modules/repos"
   repo_name   = "pulumi-inwx"
   description = "Pulumi provider for INWX that allows registration and management of domains and their domain contacts"
+  archived    = true
   github_template = {
     owner      = "pulumi"
     repository = "pulumi-tf-provider-boilerplate"
