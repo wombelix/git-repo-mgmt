dfb34e8d330efe89046dcc3d014dc8ac4f59ccad
Dominik Wombacher <dominik@wombacher.cc>
2026-06-14 00:26:24 +0200

    feat(repo): add pulp_helm
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

---
+++ repo_pulp_helm.tf
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module "pulp_helm" {
+  source      = "./modules/repos"
+  repo_name   = "pulp_helm"
+  description = "Pulp 3 plugin to host helm chart repositories. IMPORTANT: Under Development"
+}
