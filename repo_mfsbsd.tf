0a549f1f9f0cb7f4d49e0dc185e9dc9033fa4474
Dominik Wombacher <dominik@wombacher.cc>
2026-06-13 23:00:48 +0200

    feat(repo): mfsbsd friendly fork repo deleted
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_mfsbsd.tf
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Dominik Wombacher <dominik@wombacher.cc>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-module "mfsbsd" {
-  source      = "./modules/repos"
-  repo_name   = "mfsbsd"
-  description = "Friendly fork of https://github.com/mmatuska/mfsbsd to add fixes, features and automate image builds"
-}
