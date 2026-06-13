0d74874c3f798636120f21e2c2c8beee87ffdda9
Dominik Wombacher <dominik@wombacher.cc>
2026-06-14 00:49:59 +0200

    feat(repo): pulp_helm archived
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_pulp_helm.tf
+++ repo_pulp_helm.tf
@@ -6,4 +6,5 @@ module "pulp_helm" {
   source      = "./modules/repos"
   repo_name   = "pulp_helm"
   description = "Pulp 3 plugin to host helm chart repositories. IMPORTANT: Under Development"
+  archived    = true
 }
