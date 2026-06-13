ce105599c7c82a3fcbaa3ab37502eb16d79124f6
Dominik Wombacher <dominik@wombacher.cc>
2026-06-13 23:04:22 +0200

    feat(repo): moinmoin-pypy2-container archived
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_moinmoin-pypy2-container.tf
+++ repo_moinmoin-pypy2-container.tf
@@ -6,4 +6,5 @@ module "moinmoin-pypy2-container" {
   source      = "./modules/repos"
   repo_name   = "moinmoin-pypy2-container"
   description = "Ready to use MoinMoin Wiki Container Image powered by pypy2, uswgi, nginx and openSUSE Leap"
+  archived    = true
 }
