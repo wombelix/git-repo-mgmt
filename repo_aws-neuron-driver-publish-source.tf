82fd4ab91486b88ca43fb0f5758d5861bf2f40e0
Dominik Wombacher <dominik@wombacher.cc>
2026-06-13 22:45:58 +0200

    feat(repo): aws-neuron-driver-publish-source archived
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_aws-neuron-driver-publish-source.tf
+++ repo_aws-neuron-driver-publish-source.tf
@@ -6,4 +6,5 @@ module "aws-neuron-driver-publish-source" {
   source      = "./modules/repos"
   repo_name   = "aws-neuron-driver-publish-source"
   description = "CLI tool to publish the source code of new AWS Neuron Driver releases"
+  archived    = true
 }
