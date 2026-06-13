674b44c246ddfdf54fdccbd53aabcd5fde4ef6c3
Dominik Wombacher <dominik@wombacher.cc>
2026-06-13 22:52:00 +0200

    feat(repo): chart-fluent-bit-syslog-to-aws-cloudtrail-data archived
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_chart-fluent-bit-syslog-to-aws-cloudtrail-data.tf
+++ repo_chart-fluent-bit-syslog-to-aws-cloudtrail-data.tf
@@ -6,4 +6,5 @@ module "chart-fluent-bit-syslog-to-aws-cloudtrail-data" {
   source      = "./modules/repos"
   repo_name   = "chart-fluent-bit-syslog-to-aws-cloudtrail-data"
   description = "Helm Chart to receive Syslog events and push to AWS Cloudtrail Data via Fluent Bit"
+  archived    = true
 }
