46758484c95a0fc32c34bcade5746e90dd6cf945
Dominik Wombacher <dominik@wombacher.cc>
2026-06-13 23:21:50 +0200

    feat(repo): ansible-molecule-container archived
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

--- repo_ansible-molecule-container.tf
+++ repo_ansible-molecule-container.tf
@@ -6,4 +6,5 @@ module "ansible-molecule-container" {
   source      = "./modules/repos"
   repo_name   = "ansible-molecule-container"
   description = "Collection of Dockerfiles for Container to test Ansible Roles with Molecule"
+  archived    = true
 }
