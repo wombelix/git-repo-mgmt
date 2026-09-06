c02597d22397eadb2c927ac2a45eb62e4339af55
Dominik Wombacher <dominik@wombacher.cc>
2026-09-06 22:26:16 +0200

    feat(git_deploy_key): 'repo' tf that only creates AWS resources
    basically a 'dummy' repo to generate the shared git deploy key
    
    Signed-off-by: Dominik Wombacher <dominik@wombacher.cc>

---
+++ git_deploy_key.tf
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2026 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Shared git deploy key mainly for repository mirroring.
+# AWS resources only, no git actual repos are created.
+# Interim solution, to be replaced by a ssh deploy key per repo later.
+
+module "git-deploy-key" {
+  source    = "./modules/repos"
+  repo_name = "_git-deploy-key"
+
+  enable_sourcehut = false
+  enable_github    = false
+  enable_gitlab    = false
+  enable_codeberg  = false
+
+  enable_aws_ssh_key = true
+}
